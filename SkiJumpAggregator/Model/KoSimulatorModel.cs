cae869f7fbda5b945ffa1e91483271b292af363e
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2019-01-24 21:58:17 +0100

    Updated project structure to MVVM

---
+++ SkiJumpAggregator/Model/KoSimulatorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SkiJumpAggregator.Model
+{
+    public class KoSimulatorModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+    }
+}
---
+++ SkiJumpAggregator/Model/WindCalculatorModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SkiJumpAggregator.Model
+{
+    public class WindCalculatorModel : INotifyPropertyChanged
+    {
+
+        private void Formula(int HS, double avgSpeed)
+        {
+            
+        }
+
+        private void RoundUp(double score)
+        {
+            // use score from Formula()
+            
+        }
+
+        private void PointsFromHS(int HS)
+        {
+            // use switch case with pattern from wiki
+            
+        }
+
+        public double CalculateToPoints(double score)
+        {
+            // return points from RoundUp() divided by PointsFromHS()
+            return;
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
