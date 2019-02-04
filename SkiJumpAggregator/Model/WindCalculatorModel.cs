9c4a8cc21a3e2f21bfe6532887599465d84cd042
muchafilip7@gmail.com <muchafilip7@gmail.com>
2019-02-04 18:30:26 +0100

    I have completed class WindCalculatorModel

--- SkiJumpAggregator/Model/WindCalculatorModel.cs
+++ SkiJumpAggregator/Model/WindCalculatorModel.cs
@@ -5,33 +5,67 @@ namespace SkiJumpAggregator.Model
 {
     public class WindCalculatorModel : INotifyPropertyChanged
     {
+        private double score{get; set;}
+        private double points{get; set;}
 
         private void Formula(int HS, double avgSpeed)
         {
-            
+            score = avgSpeed * (HS - 36)/20;
         }
 
         private void RoundUp(double score)
         {
-            // use score from Formula()
-            
+          score += Math.Round(score * 2, MidpointRounding.AwayFromZero) / 2;
         }
-
-        private void PointsFromHS(int HS)
+        private double PointsFromHS(int HS, int K)
+       
         {
-            // use switch case with pattern from wiki
+                if (K >= 20 && K <= 25 ) {
+                    points = 4.8;
+                }
+                else if (K >= 26 && K <= 30 ){
+                    points = 4.4;
+                }
+                else if (K >= 31 && K <= 34 ){
+                    points = 4;
+                }
+                  else if (K >= 35 && K <= 39 ){
+                    points = 3.6;
+                }
+                  else if (K >= 40 && K <= 49 ){
+                    points = 3.2;
+                }
+                  else if (K >= 50 && K <= 59 ){
+                    points = 2.8;
+                }
+                  else if (K >= 60 && K <= 69 ){
+                    points =  2.4;
+                }
+                  else if (K >= 70 && K <= 79 ){
+                    points = 2.2;
+                }
+                else if (K >= 80 && K <= 99 ){
+                    points = 2.0;
+                }
+                else if (K >= 100 && K <= 169 ){
+                    points = 1.8;
+                }
+                else if (K >= 170){
+                    points = 1.2;
+                }
+           }
+            //use switch case with pattern from wiki
             
         }
 
-        /*
+        
         public double CalculateToPoints(double score)
         {
-            // return points from RoundUp() divided by PointsFromHS()
-            return;
+            //return points from RoundUp() divided by PointsFromHS();
+            return score / points;
         }
-    */
 
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
-}
+
